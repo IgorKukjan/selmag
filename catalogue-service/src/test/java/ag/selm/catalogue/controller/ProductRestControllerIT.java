9efaf907595ae89e9a596876ca1fd5d146e33bfe
Igor Kukyan <kukyan@irc.beltelecom.by>
2025-07-25 10:41:35 +0300

    findProduct

--- catalogue-service/src/test/java/ag/selm/catalogue/controller/ProductRestControllerIT.java
+++ catalogue-service/src/test/java/ag/selm/catalogue/controller/ProductRestControllerIT.java
@@ -7,12 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional//бд в исходное состояние
@@ -22,6 +24,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ProductRestControllerIT {
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    @Sql("/sql/products.sql")
+    void findProduct_ProductExists_ReturnsProduct() throws Exception {
+        //given
+        var requestBuilder = MockMvcRequestBuilders.get("/catalogue-api/products/1")
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
+
+        //when
+        this.mockMvc.perform(requestBuilder)
+
+        //then
+        .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("""
+                        {"id": 1, "title": "Товар №1", "details":  "Описание товара №1"}
+                        """));
+    }
 
     @Test
     @Sql("/sql/products.sql")
